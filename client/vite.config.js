5e1bf73f4b1138cf4e94597a7d27648583b7f7f6
jasminlei <jasminleinonen@hotmail.com>
2026-09-13 16:26:09 +0300

    add dev dockerfiles, docker-compose file and nginx conf

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    allowedHosts: ['client'],
     proxy: {
-      '/api': 'http://localhost:3001',
+      '/api': 'http://server:3001',
     },
   },
 })
